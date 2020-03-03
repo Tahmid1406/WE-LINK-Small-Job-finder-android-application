364ff4709b8cf11db41527910ccfbb197ef309ab
tahmid1406 <tahmidpranto1406@gmail.com>
2020-03-04 01:20:55 +0600

    all User Recycler

--- WeLink/app/src/main/java/com/example/welink/Fragments/AllUserFragment.java
+++ WeLink/app/src/main/java/com/example/welink/Fragments/AllUserFragment.java
@@ -1,32 +1,100 @@
 package com.example.welink.Fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.welink.MODEL.User;
 import com.example.welink.R;
+import com.example.welink.ViewHolder.UserViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AllUserFragment extends Fragment {
-
-
+    private RecyclerView recyclerview;
+    private DatabaseReference userRef;
+    private TextView AllUser;
+    private ArrayList<User> users;
+    private FirebaseRecyclerOptions<User> options;
+    private FirebaseRecyclerAdapter<User, UserViewHolder> adapter;
     public AllUserFragment() {
         // Required empty public constructor
     }
 
 
     @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_user, container, false);
+        View rootview =  inflater.inflate(R.layout.fragment_all_user, container, false);
+
+        recyclerview = (RecyclerView) rootview.findViewById(R.id.recyclerview);
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(AllUserFragment.this.getContext() ));
+        AllUser = (TextView) rootview.findViewById(R.id.AllUser);
+
+        users = new ArrayList<User>();
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        userRef.keepSynced(true);
+
+        options = new FirebaseRecyclerOptions.Builder<User>().setQuery(userRef,User.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UserViewHolder holder, int i, @NonNull User user) {
+                holder.userDisplayName.setText(user.getName());
+                Picasso.with(AllUserFragment.this.getContext()).load(user.getImage()).fit().centerInside().into(holder.userDisplayImage);
+            }
+
+            @NonNull
+            @Override
+            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new UserViewHolder(LayoutInflater.from(AllUserFragment.this.getContext()).inflate(R.layout.row, parent, false));
+            }
+        };
+
+
+        recyclerview.setAdapter(adapter);
+
+
+
+        return rootview;
     }
 
 }
--- WeLink/app/src/main/java/com/example/welink/MODEL/User.java
+++ WeLink/app/src/main/java/com/example/welink/MODEL/User.java
@@ -1,48 +1,16 @@
 package com.example.welink.MODEL;
 
 public class User {
-    String age;
-    String email;
-    String gender;
+
     String image;
     String name;
-    String occupation;
-    String password;
-    String phone;
 
-    public User(String age, String email, String gender, String image, String name, String occupation, String password, String phone) {
-        this.age = age;
-        this.email = email;
-        this.gender = gender;
+    public User() {
+    }
+
+    public User(String image, String name) {
         this.image = image;
         this.name = name;
-        this.occupation = occupation;
-        this.password = password;
-        this.phone = phone;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     public String getImage() {
@@ -59,29 +27,5 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 }
